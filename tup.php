192005fb2360e2b38558af5a2e26ec6fda2f9024
Anan5a <ananta.ap69@gmail.com>
2017-07-05 23:01:04 +0600

    Update

--- tup.php
+++ tup.php
@@ -6,10 +6,10 @@ if (isset($_POST['submit'])) {
     $t = $upl->upload($_FILES['im']);
     if (!empty($t)) {
         foreach ($t as $r) {
-            echo "<a href=\'/$r\'>$r</a>";
+            echo "<a href=\'/".$r."\'>$r</a>";
         }
     } else {
-        echo "<h1>No file uploaded!";
+        echo "<h1>No file uploaded!</h1>";
     }
    // $upl->dumpInfo();
 }
@@ -19,7 +19,7 @@ function del()
 {
     $f = glob('upload/*');
     foreach ($f as $value) {
-        if (filemtime($value)+(3600) > time()) {
+        if ( time() > filemtime($value)+(3600)) {
             unlink($value);
             printf('<br> Deleted <strong>%s</strong><br>', $value);
         }
